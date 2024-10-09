c12c32046d60789d36782fbb159cadff3ebc3023
Somraj Mukherjee <somraj.mukherjee@securly.com>
2024-10-09 21:09:04 +0530

    feat: :zap: add tabs and changed colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,13 +4,10 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen
-        name='index'
-        options={{ title: 'Home' }}
+        name='(tabs)'
+        options={{ headerShown: false }}
       />
-      <Stack.Screen
-        name='about'
-        options={{ title: 'About' }}
-      />
+      <Stack.Screen name='+not-found' />
     </Stack>
   );
 }
