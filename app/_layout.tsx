b737b858fd507e3d958a096590c604711c39d283
Somraj Mukherjee <somraj.mukherjee@securly.com>
2024-10-09 20:53:31 +0530

    feat: :zap: add about screen and about stack on layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen
+        name='index'
+        options={{ title: 'Home' }}
+      />
+      <Stack.Screen
+        name='about'
+        options={{ title: 'About' }}
+      />
     </Stack>
   );
 }
