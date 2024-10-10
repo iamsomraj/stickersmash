b24f7a981b2b7398c09d4f919ebedc69b2c692b5
Somraj Mukherjee <somraj.mukherjee@securly.com>
2024-10-10 19:59:41 +0530

    feat: :zap: add image-viewer component for HomeScreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,14 @@
-import { Link } from 'expo-router';
-import { Text, View, StyleSheet } from 'react-native';
+import ImageViewer from '@/components/image-viewer';
+import { StyleSheet, View } from 'react-native';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link
-        style={styles.button}
-        href={'/about'}
-      >
-        Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
     </View>
   );
 }
@@ -22,12 +20,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
   },
 });
