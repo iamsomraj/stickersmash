55019e34660fe06b4248955a2fd05a4380aff257
Somraj Mukherjee <iamsomraj@gmail.com>
2024-10-12 13:43:09 +0530

    refactor: improve header styling and add developer link in AboutScreen component

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,19 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.header}>Sticker Smash</Text>
+      <Text style={styles.text}>Developed by</Text>
+      <Link
+        style={styles.link}
+        href='https://github.com/iamsomraj'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Somraj Mukherjee
+      </Link>
     </View>
   );
 }
@@ -14,8 +24,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     justifyContent: 'center',
     alignItems: 'center',
+    gap: 10,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff',
   },
   text: {
     color: '#fff',
+  },
+  link: {
+    color: '#61dafb',
   },
 });
